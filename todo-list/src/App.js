a0a697458230c68cdf8068cd61060e103ad0e7f1
Jan Krysztop <jankrysztop@gmail.com>
2022-01-11 16:03:36 +0100

    HTML structure and CSS styling added

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,10 +1,32 @@
+import  style  from './index.module.css';
 
 
 function App() {
   return (
-    <div className="App">
-      test test raz dwa trzy
-    </div>
+    <>
+      <div className={style.todo}>
+        <header className={style.header}>
+          <h1>todos</h1>
+          <input type="checkbox" />
+          <input className={style.input}  name="name" placeholder="What needs to be done?" />
+        </header>
+        <section className={style.section}>
+          <ul>
+            <li className={style.list}>
+                <input type={style.checkbox}/>
+                <label>fffff</label>
+                <button className={style.destroy}></button>
+            </li>
+          </ul>
+        </section>
+        <footer className={style.footer}>
+          <p className={style.todoCount}><span>0</span> items left</p>
+          <button>All</button>
+          <button>Active</button>
+          <button>Completed</button>
+        </footer>
+      </div>
+    </>
   );
 }
 
